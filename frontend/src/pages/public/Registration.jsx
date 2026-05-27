1506c7b3fe7b191976bb35a8f13f6c1c57bb3c01
nabilaghnaaa <nabilaghnaaa@gmail.com>
2026-05-27 22:03:49 +0700

    menghapus option SMA di registration

--- frontend/src/pages/public/Registration.jsx
+++ frontend/src/pages/public/Registration.jsx
@@ -754,7 +754,6 @@ export default function Registration() {
                         error={errors.pendidikan_terakhir}
                       >
                         <option value="">Pilih Pendidikan Terakhir</option>
-                        <option value="SMA/SMK">SMA/SMK</option>
                         <option value="D3">D3</option>
                         <option value="D4">D4</option>
                         <option value="S1">S1</option>
